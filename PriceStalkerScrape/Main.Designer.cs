753c38c5473d48d2d01f90a39a249ad006e84d2c
kgeorgiadis <kgeorgiadis@datateam.gr>
2022-04-20 16:00:49 +0300

    Add project files.

---
+++ PriceStalkerScrape/Main.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace PriceStalkerScrape
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.lblProductTitle = new System.Windows.Forms.Label();
+            this.lblProductPrice = new System.Windows.Forms.Label();
+            this.lblProductRating = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.cbProducts = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblProductTitle
+            // 
+            this.lblProductTitle.AutoSize = true;
+            this.lblProductTitle.Location = new System.Drawing.Point(40, 31);
+            this.lblProductTitle.Name = "lblProductTitle";
+            this.lblProductTitle.Size = new System.Drawing.Size(0, 20);
+            this.lblProductTitle.TabIndex = 9;
+            // 
+            // lblProductPrice
+            // 
+            this.lblProductPrice.AutoSize = true;
+            this.lblProductPrice.Location = new System.Drawing.Point(40, 64);
+            this.lblProductPrice.Name = "lblProductPrice";
+            this.lblProductPrice.Size = new System.Drawing.Size(0, 20);
+            this.lblProductPrice.TabIndex = 10;
+            // 
+            // lblProductRating
+            // 
+            this.lblProductRating.AutoSize = true;
+            this.lblProductRating.Location = new System.Drawing.Point(114, 64);
+            this.lblProductRating.Name = "lblProductRating";
+            this.lblProductRating.Size = new System.Drawing.Size(0, 20);
+            this.lblProductRating.TabIndex = 11;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(8, 167);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.ReadOnly = true;
+            this.txtDescription.Size = new System.Drawing.Size(821, 315);
+            this.txtDescription.TabIndex = 12;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tabControl1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 128);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1514, 532);
+            this.panel1.TabIndex = 13;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1514, 529);
+            this.tabControl1.TabIndex = 13;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.materialFlatButton1);
+            this.tabPage1.Controls.Add(this.lblProductRating);
+            this.tabPage1.Controls.Add(this.lblProductTitle);
+            this.tabPage1.Controls.Add(this.txtDescription);
+            this.tabPage1.Controls.Add(this.lblProductPrice);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 33);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1506, 492);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Scrap Tab";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Location = new System.Drawing.Point(1324, 15);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = true;
+            this.materialFlatButton1.Size = new System.Drawing.Size(174, 53);
+            this.materialFlatButton1.TabIndex = 13;
+            this.materialFlatButton1.Text = "Import To Database";
+            this.materialFlatButton1.UseVisualStyleBackColor = true;
+            this.materialFlatButton1.Click += new System.EventHandler(this.materialFlatButton1_Click_1);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dgvProducts);
+            this.tabPage2.Location = new System.Drawing.Point(4, 33);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1506, 492);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Database";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvProducts.Location = new System.Drawing.Point(3, 3);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.ReadOnly = true;
+            this.dgvProducts.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProducts.Size = new System.Drawing.Size(1500, 486);
+            this.dgvProducts.TabIndex = 0;
+            this.dgvProducts.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProducts_CellDoubleClick);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.cbProducts);
+            this.tabPage3.Controls.Add(this.chart1);
+            this.tabPage3.Controls.Add(this.button1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 33);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1506, 492);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Dashboard";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // txtLink
+            // 
+            this.txtLink.Location = new System.Drawing.Point(12, 81);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(708, 20);
+            this.txtLink.TabIndex = 14;
+            this.txtLink.Text = "https://www.skroutz.gr/s/31019956/Xiaomi-Redmi-Buds-3-Pro-Bluetooth-Handsfree-%CE" +
+    "%9C%CE%B1%CF%8D%CF%81%CE%BF.html";
+            // 
+            // materialRaisedButton1
+            // 
+            this.materialRaisedButton1.Depth = 0;
+            this.materialRaisedButton1.Location = new System.Drawing.Point(726, 75);
+            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton1.Name = "materialRaisedButton1";
+            this.materialRaisedButton1.Primary = true;
+            this.materialRaisedButton1.Size = new System.Drawing.Size(100, 30);
+            this.materialRaisedButton1.TabIndex = 16;
+            this.materialRaisedButton1.Text = "Scrape";
+            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            this.materialRaisedButton1.Click += new System.EventHandler(this.materialRaisedButton1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(599, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(8, 70);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(574, 234);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // cbProducts
+            // 
+            this.cbProducts.FormattingEnabled = true;
+            this.cbProducts.Location = new System.Drawing.Point(8, 19);
+            this.cbProducts.Name = "cbProducts";
+            this.cbProducts.Size = new System.Drawing.Size(574, 32);
+            this.cbProducts.TabIndex = 2;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1514, 660);
+            this.Controls.Add(this.materialRaisedButton1);
+            this.Controls.Add(this.txtLink);
+            this.Controls.Add(this.panel1);
+            this.Name = "Main";
+            this.Sizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load_1);
+            this.panel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblProductTitle;
+        private System.Windows.Forms.Label lblProductPrice;
+        private System.Windows.Forms.Label lblProductRating;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private MaterialSkin.Controls.MaterialRaisedButton materialFlatButton1;
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox cbProducts;
+    }
+}
+
---
+++ PriceStalkerScrape/Main.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using HtmlAgilityPack;
+using LiveCharts;
+using LiveCharts.Wpf;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace PriceStalkerScrape
+{
+    public partial class Main : MaterialForm
+    {
+        public Main()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+        private void Initialize(string title, string price, string rating, string summary)
+        {
+            lblProductPrice.Text = price;
+            lblProductTitle.Text = title;
+            lblProductRating.Text = rating;
+            txtDescription.Text = summary;
+        }
+        private void LoadData()
+        {
+            var stalkerEntities = new Data.StalkerEntities();
+            //var .
+            dgvProducts.DataSource = stalkerEntities.tblProducts.ToList();
+        }
+        private void InsertIntoDb()
+        {
+            try
+            {
+                using (var context = new Data.StalkerEntities())
+                {
+                    Data.tblProducts product = new Data.tblProducts();
+
+                    if (context.tblProducts.Any(x => x.Link == txtLink.Text)) return;
+                    product.Link = txtLink.Text;
+                    product.Title = lblProductTitle.Text;
+                    string ignoreSign = lblProductPrice.Text.ToString().Replace("€", "").Trim();
+                    product.Price = float.Parse(ignoreSign);
+                    string rating = lblProductRating.Text.Replace(".", ",");
+                    product.Rating = float.Parse(rating);
+                    product.Description = txtDescription.Text;
+                    context.tblProducts.Add(product);
+                    context.SaveChanges();
+                    new ToastContentBuilder()
+                    .AddArgument("action", "viewConversation")
+                    .AddArgument("conversationId", 123)
+                    .AddText("Record with title " + lblProductTitle.Text + " Successfully Saved")
+                    .Show();
+                }
+            }
+            catch (System.Data.Entity.Validation.DbEntityValidationException ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Main_Load_1(object sender, EventArgs e)
+        {
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);   
+        }
+
+        private void btnScrape_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void materialFlatButton1_Click_1(object sender, EventArgs e)
+        {
+            InsertIntoDb();
+        }
+
+        private void dgvProducts_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DialogResult result = System.Windows.Forms.MessageBox.Show("Are you sure you wanna delete this record ?", "Delete ", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                string Link = dgvProducts.SelectedRows[0].Cells["Link"].Value.ToString();
+                Console.WriteLine(Link);
+                var stalkerEntities = new Data.StalkerEntities();
+                var selected = stalkerEntities.tblProducts.FirstOrDefault(x => x.Link == Link);
+                stalkerEntities.tblProducts.Remove(selected);
+                stalkerEntities.SaveChanges();
+                dgvProducts.DataSource = stalkerEntities.tblProducts.ToList();
+            }
+        }
+
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtLink.Text.StartsWith("https://www.skroutz.gr/"))
+                {
+                    // From Web 
+                    var url = txtLink.Text;
+                    var web = new HtmlWeb();
+                    var doc = web.Load(url);
+                    var title = doc?.DocumentNode?.SelectSingleNode("//h1[@class='page-title']")?.InnerText;
+                    var prices = doc?.DocumentNode?.SelectNodes("//strong[@class='dominant-price']")?.ToList();
+                    string price = prices?.FirstOrDefault().InnerText.ToString();
+                    var rating = doc?.DocumentNode?.SelectSingleNode("//span[@itemprop='ratingValue']")?.InnerText; //actual-rating 
+                                                                                                                    //var summary = doc?.DocumentNode?.SelectNodes("//div[contains(@class,'simple-description')]/ul/li").ToList();
+                    var summary = doc?.DocumentNode?.SelectNodes("//div[contains(@class,'summary')]")?.ToList(); //summary
+                    string description = "";
+                    if (summary != null)
+                    {
+                        foreach (var s in summary)
+                        {
+                            description += s.InnerText;
+                        }
+                    }
+                    if (title != null && price != null && rating != null & summary != null)
+                    {
+                        Initialize(title, price.ToString(), rating.ToString(), description);
+                    }
+                }
+            }
+            catch (System.NullReferenceException ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Add("test");
+            //chart1.Series.Add("test2");
+            //chart1.Series["test"].Points.AddXY("test1",12);
+            //chart1.Series["test2"].Points.AddXY("test2", 16);
+            
+            using (var context = new Data.StalkerEntities())
+            {
+                var data = context.PriceHistory.Where(x=>x.PId==7).ToList();
+                foreach (var item in data)
+                {
+                    chart1.Series["test"].Points.AddXY(item.Date.ToString(), item.Price);
+                }
+            }
+        }
+    }
+}
